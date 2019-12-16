10b585b161eaddfc17d08590c6b4416ecd6fb3d4
miyagin-tra <miya15work@yahoo.co.jp>
2019-12-16 22:29:09 +0900

    キャリブレーションの座標と通常の座標の値をcsvに保存するようにした

--- ipad_scrollapp/ViewController.swift
+++ ipad_scrollapp/ViewController.swift
@@ -669,6 +669,50 @@ class ViewController: UIViewController, ARSCNViewDelegate, UITableViewDelegate,U
             self.goalLabel.text="データがありません。"
             return
         }
+        //キャリブレーション座標のラベル追加
+        for x in 0...11{
+            if(x != 11){
+                fileStrData+=String(callibrationArr[x])+","
+            }else{
+                fileStrData+=String(callibrationArr[x])+"\n"
+            }
+        }
+        //キャリブレーションMAX座標の値
+        for x in 0...9{
+            if(x != 9){
+                if let value = userDefaults.string(forKey: callibrationArr[x]) {
+                    fileStrData+=String(value)+","
+                }else{
+                    print("no value",x)
+                }
+            }else{
+                if let value = userDefaults.string(forKey: callibrationArr[x]) {
+                    fileStrData+=String(value)+"\n"
+                }else{
+                    print("no value",x)
+                }
+            }
+        }
+        //普通の時のラベル
+        for x in 0...11{
+            if(x != 11){
+                fileStrData+=String("普通"+callibrationArr[x])+","
+            }else{
+                fileStrData+=String("普通"+callibrationArr[x])+"\n"
+            }
+        }
+        //普通の時の座標
+        for x in 0...11{
+            callibrationOrdinalPosition[x]=userDefaults.float(forKey: "普通"+callibrationArr[x])
+            if(x != 11){
+                fileStrData+=String(callibrationOrdinalPosition[x])+","
+            }else{
+                fileStrData+=String(callibrationOrdinalPosition[x])+"\n"
+            }
+        }
+        
+        
+        fileStrData += "position,goalPosition\n"
         for i in 1...fileArrData.count{
             if (i%2 != 0){
                 fileStrData += String(fileArrData[i-1]) + ","
